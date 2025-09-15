7f47c2ce060f07058d19313a344afcbfc0cd35a1
Adebayo Sotannde <me@adebayosotannde.com>
2025-09-14 21:22:10 -0500

    Initial Commit

---
+++ Tally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally
+//
+//  Created by Developer on 9/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
