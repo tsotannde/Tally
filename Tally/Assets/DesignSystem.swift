3a52620f340548dc88991cbe7078fc628acb566e
Adebayo Sotannde <me@adebayosotannde.com>
2025-09-15 00:10:08 -0500

    Work in progress working on SimpliedAuthKit to handle sign in

--- Tally/Assets/DesignSystem.swift
+++ Tally/Assets/DesignSystem.swift
@@ -5,4 +5,17 @@
 //  Created by Adebayo Sotannde on 9/14/25.
 //
 
-import Foundation
+import UIKit
+
+enum DesignSystem
+{
+    enum AppColors {
+        static let primary = UIColor(named: "Primary")!
+        static let secondary = UIColor(named: "Secondary")!
+        static let error = UIColor(named: "Error")!
+        static let success = UIColor(named: "Success")!
+        static let textPrimary = UIColor(named: "Text Primary")!
+        static let textSecondary = UIColor(named: "Text Secondary")!
+        static let background = UIColor(named: "Background")!
+    }
+}
